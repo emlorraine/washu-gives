1476cfb8d97ec608c61318c66c977063e10c17fa
Mauricio Fossas Koval <mauriciofossaskoval@Mauricios-MacBook-Air.local>
2020-09-26 16:54:01 -0500

    Basic login form and ability to host in aws

---
+++ WashU_Gives/src/app/app.module.ts
@@ -0,0 +1,21 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { appRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule,
+  ],
+  declarations: [AppComponent, LoginComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ WashU_Gives/src/app/app.routing.ts
@@ -0,0 +1,7 @@
+﻿import { Routes, RouterModule } from "@angular/router";
+
+import { LoginComponent } from "./login";
+
+const routes: Routes = [{ path: "login", component: LoginComponent }];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
