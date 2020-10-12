5249b61ae0196cc29aceeac8816f3d9c2aebca3c
Emma Baker <emma.baker@wustl.edu>
2020-10-11 19:53:17 -0500

    Feat: auth components

--- WashU_Gives/src/app/app.module.ts
+++ WashU_Gives/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from "./login";
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 @NgModule({
   imports: [
@@ -24,4 +25,14 @@ import { environment } from './environments/environment';
   providers: [],
   bootstrap: [AppComponent],
 })
+
+@NgModule({
+  imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+  ]
+})
+
 export class AppModule {}
+
