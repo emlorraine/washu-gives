5f4d417991847d78ae19d9988ff6fdada40b3f33
Mauricio Fossas Koval <mauriciofossaskoval@Mauricios-MacBook-Air.local>
2020-10-20 18:26:26 -0500

    Added folder

---
+++ washugives_new/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+@Component({ selector: 'home-component', templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  openNav() {
+    document.getElementById('mySidebar').style.width = '25%';
+    document.getElementById('main').style.marginLeft = '25%';
+  }
+
+  closeNav() {
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+}
