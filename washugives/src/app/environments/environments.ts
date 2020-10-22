8b77fe39d4d6acca5eefe0ea330f354ceb7c76d1
Emma Baker <emma.baker@wustl.edu>
2020-10-22 02:01:15 -0500

    Create environments.ts

---
+++ washugives/src/app/environments/environments.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDk9DOq-MEltxUk_wUCU1vCVdx6irwAZIw",
+  authDomain: "washu-gives-e891d.firebaseapp.com",
+  databaseURL: "https://washu-gives-e891d.firebaseio.com",
+  projectId: "washu-gives-e891d",
+  storageBucket: "washu-gives-e891d.appspot.com",
+  messagingSenderId: "659039917754",
+  appId: "1:659039917754:web:4b9f05951a3c00177e1483",
+  measurementId: "G-CDX36MBBPY"
+};
