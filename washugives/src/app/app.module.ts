39bdedce9f6f8b32e2660674d38badf227d67022
Emma Baker <emma.baker@wustl.edu>
2020-10-22 16:51:45 -0500

    Feat: enabled firebase auth

--- washugives/src/app/app.module.ts
+++ washugives/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,9 +11,15 @@ import { HeaderComponent } from './header/header.component';
 import { AppFormComponent } from './app-form/app-form.component';
 import { FilterFormComponent } from './filter-form/filter-form.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent, HeaderComponent, AppFormComponent, FilterFormComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule,   AngularFireModule.initializeApp(environment.firebase),
+  AngularFirestoreModule, AngularFireAuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- washugives/src/environments/environment.ts
+++ washugives/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebase: {
+    apiKey: "AIzaSyDk9DOq-MEltxUk_wUCU1vCVdx6irwAZIw",
+    authDomain: "washu-gives-e891d.firebaseapp.com",
+    databaseURL: "https://washu-gives-e891d.firebaseio.com",
+    projectId: "washu-gives-e891d",
+    storageBucket: "washu-gives-e891d.appspot.com",
+    messagingSenderId: "659039917754",
+    appId: "1:659039917754:web:4b9f05951a3c00177e1483",
+    measurementId: "G-CDX36MBBPY"
+    },
 };
 
 /*
