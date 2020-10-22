849962ebfb687c58ecc8b82224ab905faf7ee198
Emma Baker <emma.baker@wustl.edu>
2020-10-22 02:13:23 -0500

    Feat: Routing

--- washugives/src/app/app-routing.module.ts
+++ washugives/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegistrationComponent },
 ];
 
 @NgModule({
---
+++ washugives/src/app/registration/registration.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+@Component({ selector: 'registration_component', templateUrl: 'registration.component.html' })
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registrationForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+  }
+}
